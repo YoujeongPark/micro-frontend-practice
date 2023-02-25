57369950ad7d3c178ee51c8b5c409a354bc62284
Youjeong Park <pyj0695@gmail.com>
2023-02-25 18:02:33 +0900

    #1 #3  microfrontend 구축

---
+++ home/src/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Header() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+      Header - 2
+    </div>
+  );
+}
